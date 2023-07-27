0827ce18e72f91ffc32ad059067e2bf75f0fad02
PonnarasanRK <140476219+PonnarasanRK@users.noreply.github.com>
2023-07-27 14:53:04 +0530

    Update Counter.js

--- Counter.js
+++ Counter.js
@@ -1,24 +1,26 @@
-import React from 'react'
+import {useState} from 'react'
 
 function Counter() {
-    let count =10;
 
+    const [count,setCount]= useState(0);
     const handleAdd=()=>{
-        console.log("+ clicked");
+        setCount(count+1);
+        console.log(count);
     }
 
     const handleSub=()=>{
-        console.log("- clicked");
+        setCount(count-1);
+        console.log(count);
     }
 
   return (
     <div>
     <h2>Counter</h2>
-    <h3>0</h3>
+    <h3>{count}</h3>
     <button onClick={handleSub} style={{padding:8,backgroundColor:"#045590",margin:4,color:"white"}}>-</button>
     <button onClick={handleAdd} style={{padding:8,backgroundColor:"#045590",margin:4,color:"white"}}>+</button>
     </div>
   )
 }
 
-export default Counter
+export default Counter 
